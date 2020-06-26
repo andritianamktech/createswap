4741a9650fb49a85b1c56f1431d1e093384f86c3
RANDRIANJATOVO Andritiana <36677588+andritianamktech@users.noreply.github.com>
2020-06-26 23:06:50 +0300

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -1,11 +1,15 @@
-#--CREATE SWAP PARTITION--#
-dd if=/dev/zero of=/swapfile bs=1024 count=1024k #read from /dev/zero write to /swapfile r/w up to 1024 bytes copy only 1024k blocks
+dd if=/dev/zero of=/swapfile bs=1024 count=1024k 
+#read from /dev/zero write to /swapfile r/w up to 1024 bytes copy only 1024k blocks
 chown root:root /swapfile #change file owner /swapfile to root user in root group
-chmod 0600 /swapfile #add permission 600
-mkswap /swapfile #setup a linux swap area i.e on /swapfile
-swapon /swapfile #enable devices and files for paging and swapping
-echo "/swapfile          swap            swap    defaults        0 0" >> /etc/fstab #make swapfile visible in /etc/fstab
-sysctl vm.swappiness=10 #tell the kernel to use swap when the memory usage is up to 90%
-echo vm.swappiness=10 >> /etc/sysctl.conf #write it at the end of /etc/sysctl.conf
-free -h
-cat /proc/sys/vm/swappiness
+chmod 0600 /swapfile 
+#add permission 600
+mkswap /swapfile 
+#setup a linux swap area i.e on /swapfile
+swapon /swapfile 
+#enable devices and files for paging and swapping
+echo "/swapfile          swap            swap    defaults        0 0" >> /etc/fstab 
+#make swapfile visible in /etc/fstab
+sysctl vm.swappiness=10 
+#tell the kernel to use swap when the memory usage is up to 90%
+echo vm.swappiness=10 >> /etc/sysctl.conf 
+#write it at the end of /etc/sysctl.conf
